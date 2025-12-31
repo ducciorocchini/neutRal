8fee0d352b7fe5aa304994356c74ba91b7247144
Duccio Rocchini <ducciorocchini@gmail.com>
2025-12-31 10:55:50 +0100

    Delete R/calculate_competition_diversity.R

--- R/calculate_competition_diversity.R
+++
@@ -1,9 +0,0 @@
-# Function to calculate biodiversity metrics (richness, total individuals, Shannon diversity)
-calculate_competition_diversity <- function(grid) {
-  tab <- table(as.vector(grid))
-  S <- length(tab)                # Richness (number of species)
-  N <- sum(tab)                   # Total individuals
-  H <- -sum((tab/N) * log(tab/N)) # Shannon diversity
-
-  return(list(Richness = S, Total = N, Shannon = round(H, 3)))
-}
