9f59db740fe58a767ac78e3a58ee0b0072c797a3
Duccio Rocchini <ducciorocchini@gmail.com>
2025-12-31 16:05:42 +0100

    Update plot_neutral_grid.R

--- R/plot_neutral_grid.R
+++ R/plot_neutral_grid.R
@@ -1,3 +1,33 @@
+#' Plot a spatially explicit neutral community
+#'
+#' This function visualizes the spatial configuration of a simulated neutral
+#' community on a square lattice. Each cell in the grid is colored according
+#' to its species identity. Simulation parameters are used only to annotate
+#' the plot and do not affect the graphical computation.
+#'
+#' @param grid A matrix representing the final community state, where each
+#'   cell contains a species identifier.
+#' @param n An integer specifying the number of cells along each side of the
+#'   square grid.
+#' @param steps The number of simulation steps used to generate the grid.
+#'   This parameter is used for plot annotation only.
+#' @param m The immigration probability used in the simulation. This parameter
+#'   is used for plot annotation only.
+#' @param nu The speciation probability (conditional on immigration) used in
+#'   the simulation. This parameter is used for plot annotation only.
+#'
+#' @return A \code{ggplot} object representing the spatial distribution of
+#'   species across the grid.
+#'
+#' @details
+#' The function does not modify the input grid and does not perform any
+#' simulation. All parameters except \code{grid} are included solely to provide
+#' informative annotations in the resulting plot.
+#'
+#' @seealso \code{\link{simulate_neutral_grid}}, \code{\link{calculate_diversity}}
+#'
+#' @importFrom ggplot2 ggplot aes geom_raster coord_equal labs theme_minimal theme
+#' @export
 plot_neutral_grid <- function(grid, n, steps, m, nu) {
 
   df <- expand.grid(x = 1:n, y = 1:n)
